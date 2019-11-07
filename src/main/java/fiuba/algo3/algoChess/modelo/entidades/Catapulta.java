6e80782ce8de29a31ca47c86012fa7dd67b5478c
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 14:59:40 -0300

    Update Catapulta.java

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/Catapulta.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/Catapulta.java
@@ -1,14 +1,16 @@
 package fiuba.algo3.algoChess.modelo.entidades;
 import fiuba.algo3.algoChess.modelo.ataque.*;
+import fiuba.algo3.algoChess.modelo.jugador.*;
 
 public class Catapulta extends Unidad{
 	
-	public Catapulta() {
+	public Catapulta(Jugador jugador) {
 		
 	
 		vida = 50;
 		ataque = new AtaqueDistanciaLejana(20);
 		costo = 5;
+		this.setDuenio(jugador);
 	}
 	
 	
