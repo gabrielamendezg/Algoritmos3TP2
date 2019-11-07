bde7030e5fa73694e8c7569237614dd8bc9c91e4
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 04:17:02 -0300

    Add files via upload

---
+++ src/test/java/fiuba/algo3/algoChess/modelo/entidades/CuranderoCuraVidaCorrespondiente.java
@@ -0,0 +1,32 @@
+package fiuba.algo3.algoChess.modelo.entidades;
+
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CuranderoCuraVidaCorrespondiente {
+	
+	Curandero curandero = new Curandero();
+	
+	SoldadoDeInfanteria soldado = new SoldadoDeInfanteria ();
+	Jinete jinete = new Jinete();
+	
+
+	@Test
+	void test() {
+		
+		soldado.setPosicion(1,1);
+		jinete.setPosicion(1, 2);
+		
+		curandero.setPosicion(1, 5);
+		
+		soldado.atacar(jinete);
+		soldado.atacar(jinete);
+		
+		curandero.atacar(jinete);
+		assertTrue(jinete.obtenerVida() == 95);
+	}
+
+}
