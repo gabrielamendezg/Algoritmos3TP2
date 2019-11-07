05974e1800d96d3f51d99b391e3e0371f8a21f8e
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 14:57:39 -0300

    Update SoldadoDeInfanteria.java

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/SoldadoDeInfanteria.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/SoldadoDeInfanteria.java
@@ -1,13 +1,16 @@
 package fiuba.algo3.algoChess.modelo.entidades;
 
+import fiuba.algo3.algoChess.modelo.jugador.*;
+
 import fiuba.algo3.algoChess.modelo.ataque.*;
 
 public class SoldadoDeInfanteria extends Unidad{
 	
-	public SoldadoDeInfanteria() {
+	public SoldadoDeInfanteria(Jugador jugador) {
 		ataque = new AtaqueCercano(10);
 		vida = 100;
 		costo = 1;
+		this.setDuenio(jugador);
 	}
 	
 }
