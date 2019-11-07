e335e537de7c804eae6e5a307741dfc416063f83
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 02:32:16 -0300

    Update CeldaVacia.java
    cambio a Unidad

--- src/main/java/fiuba/algo3/algoChess/tablero/CeldaVacia.java
+++ src/main/java/fiuba/algo3/algoChess/tablero/CeldaVacia.java
@@ -1,18 +1,18 @@
 package fiuba.algo3.algoChess.tablero;
 
-import fiuba.algo3.algoChess.modelo.entidades.Entidad;
+import fiuba.algo3.algoChess.modelo.entidades.Unidad;
 
 public class CeldaVacia extends EstadoCelda{
 
 	@Override
-	public boolean recibirEntidad(Entidad contenido, Celda receptora) {
-		receptora.setEntidad(contenido);
+	public boolean recibirUnidad(Unidad contenido, Celda receptora) {
+		receptora.setUnidad(contenido);
 		receptora.setEstado(new CeldaOcupada());
 		return true;
 	}
 
 	@Override
-	public boolean moverEntidad(Entidad contenido, Celda receptora,Celda emisora) {
+	public boolean moverUnidad(Unidad contenido, Celda receptora,Celda emisora) {
 		return false;
 	}
 	
