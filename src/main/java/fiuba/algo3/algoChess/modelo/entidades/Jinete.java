3d0434d8c47cfe0a381ad1de136de1c45042001c
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 14:58:05 -0300

    Update Jinete.java

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/Jinete.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/Jinete.java
@@ -1,12 +1,15 @@
 package fiuba.algo3.algoChess.modelo.entidades;
 
 import fiuba.algo3.algoChess.modelo.ataque.*;
+import fiuba.algo3.algoChess.modelo.jugador.*;
+
 public class Jinete extends Unidad{
 	
-	public Jinete () {
+	public Jinete (Jugador jugador) {
 		ataque = new AtaqueCercano(5);
 		vida = 100;
 		costo = 3;
+		this.setDuenio(jugador);
 		
 	}
 }
