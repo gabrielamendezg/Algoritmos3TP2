c20f276381b32ddbd6f4c8a56da9ca6eadb92471
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 14:59:12 -0300

    Update Curandero.java

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/Curandero.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/Curandero.java
@@ -1,12 +1,14 @@
 package fiuba.algo3.algoChess.modelo.entidades;
 import fiuba.algo3.algoChess.modelo.ataque.*;
+import fiuba.algo3.algoChess.modelo.jugador.*;
 
 public class Curandero extends Unidad{
 	
-	public Curandero() {
+	public Curandero(Jugador jugador) {
 		vida = 75;
 		ataque = new AtaqueDistanciaMedia(15);
 		costo = 2;
+		this.setDuenio(jugador);
 	}
 	
 	@Override
