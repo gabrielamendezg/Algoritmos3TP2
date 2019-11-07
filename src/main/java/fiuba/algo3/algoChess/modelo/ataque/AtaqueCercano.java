3e750c3955d390c190fab53dce1992b29b517f31
PurrEvil <38872131+PurrEvil@users.noreply.github.com>
2019-11-07 02:56:53 -0300

    Add files via upload

---
+++ src/main/java/fiuba/algo3/algoChess/modelo/ataque/AtaqueCercano.java
@@ -0,0 +1,12 @@
+package fiuba.algo3.algoChess.modelo.ataque;
+
+public class AtaqueCercano extends Ataque{
+	
+	public AtaqueCercano(int daño){
+		
+		rangoInicial = 1;
+		rangoFinal = 2;
+		ataque = daño;
+	}
+
+}
