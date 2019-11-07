629333a6d2179099b4dd81bc07b047af7a81df07
hernan <hernan@hernan-VAW70>
2019-11-07 18:40:24 -0300

    Va cambio que les mande recien por wasap del test

--- src/test/java/fiuba/algo3/algoChess/modelo/entidades/SoldadoNoPuedeMoverseACeldaOcupadaTest.java
+++ src/test/java/fiuba/algo3/algoChess/modelo/entidades/SoldadoNoPuedeMoverseACeldaOcupadaTest.java
@@ -18,9 +18,12 @@ public class SoldadoNoPuedeMoverseACeldaOcupadaTest {
 		void test() {
 			
 			celda1.setUnidad(soldado); //agrega entidad y setea posicion
+			celda1.recibirUnidad(soldado);
 			soldado.setPosicion(1,1);
 			
 			celda2.setUnidad(curandero); // agregaentidad y setea posicion
+			
+			
 			
 			if(celda1.recibirUnidad(curandero)) {
 				soldado.moverUnidadA(jugador,1,2);
