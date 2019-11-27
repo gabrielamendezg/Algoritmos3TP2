5577ee4c9af729f713c5813e4ee6cf61e5c1efff
Elvis <eclaros@fi.uba.ar>
2019-11-27 14:20:42 -0300

    commit para un merge parcial con master [ci skip]

--- src/test/java/fiuba/algo3/algoChess/modelo/entidades/catapulta/CatapultaTest.java
+++ src/test/java/fiuba/algo3/algoChess/modelo/entidades/catapulta/CatapultaTest.java
@@ -35,14 +35,37 @@ public class CatapultaTest {
 
     @Test
     public void catapultaAtacaAlAtacableYALosAtacablesContiguasTest(){
-     /*   JugadorA jugador = new JugadorA();
+       /* JugadorA jugador = new JugadorA();
+        JugadorB jugadorB = new JugadorB();
+
+        LinkedList atacables = new <Atacable>LinkedList();
+
+        SoldadoDeInfanteria [] soldado = new SoldadoDeInfanteria[20];
+        for (int i = 1; i <= 20; i++){
+            soldado[i] = jugadorB.elegirSoldado();
+        }
+
+        soldado[1].setPosicion(new Posicion(8, 9));soldado[6].setPosicion(new Posicion(12, 9));
+        soldado[2].setPosicion(new Posicion(9, 10));soldado[7].setPosicion(new Posicion(11, 10));
+        soldado[3].setPosicion(new Posicion(10, 11));soldado[8].setPosicion(new Posicion(9, 12));
+        soldado[4].setPosicion(new Posicion(11, 12));soldado[9].setPosicion(new Posicion(8, 13));
+        soldado[5].setPosicion(new Posicion(12, 13));soldado[10].setPosicion(new Posicion(10, 9));
+
+        soldado[11].setPosicion(new Posicion(10, 10));soldado[16].setPosicion(new Posicion(11, 11));
+        soldado[12].setPosicion(new Posicion(10, 12));soldado[17].setPosicion(new Posicion(12, 11));
+        soldado[13].setPosicion(new Posicion(10, 13));soldado[18].setPosicion(new Posicion(8, 15));
+        soldado[14].setPosicion(new Posicion(8, 11));soldado[19].setPosicion(new Posicion(9, 15));
+        soldado[15].setPosicion(new Posicion(9, 11));soldado[20].setPosicion(new Posicion(10, 15));
+
+        for (int i = 1; i <= 20; i++){
+            atacables.add(soldado[i]);
+        }
+
         Catapulta catapulta = new Catapulta(jugador);
         catapulta.setPosicion(new Posicion(1,1));
-        LinkedList atacables = new <Atacable>LinkedList();
-        SoldadoDeInfanteria soldadoDeInfanteria = new SoldadoDeInfanteria(new JugadorB());
-        soldadoDeInfanteria.setPosicion(new Posicion(11, 11));
-        atacables.add(soldadoDeInfanteria);
-        catapulta.atacarAtacable(new JugadorA(), soldadoDeInfanteria, atacables);
-        assertEquals(soldadoDeInfanteria.obtenerVida(), 80);*/
+
+
+        catapulta.atacarAtacable(new JugadorA(), soldado[1], atacables);
+        assertEquals(soldado[1].obtenerVida(), 80);*/
     }
 }
