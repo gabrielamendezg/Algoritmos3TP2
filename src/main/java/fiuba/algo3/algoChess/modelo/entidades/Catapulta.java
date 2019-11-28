41263eb203e0cf784c863a6514a6b1a5ee289ee1
Elvis <eclaros@fi.uba.ar>
2019-11-28 11:02:51 -0300

    catapulta v-01 ya pasa los test faltaria refactorizar

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/Catapulta.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/Catapulta.java
@@ -32,12 +32,15 @@ public class Catapulta extends Unidad implements Posicionable, Atacador, Atacabl
 	@Override
 	public void atacarAtacable(Jugador atacante, Atacable unAtacable, LinkedList <Atacable> atacables) {
 		ArrayList aliados = atacante.obtenerUnidades();
+
 		if(aliados.contains(unAtacable)) return;
+
 		ataque.atacar(posicion.getX(), posicion.getY(), unAtacable);
         HashMap<String, Atacable> atacableHashMap = new HashMap<>();
 		atacables.stream().forEach(atacable -> {
 		    atacableHashMap.put(atacable.getPosicion().toString(), atacable);
 		});
+		//Ataco atras segun mi perspectiva
 		int i = unAtacable.getPosicion().getY() + 1;
 		Atacable atacable = atacableHashMap.get(new Posicion(unAtacable.getPosicion().getX(), i).toString());
 		while (atacable != null){
@@ -45,5 +48,76 @@ public class Catapulta extends Unidad implements Posicionable, Atacador, Atacabl
             i++;
             atacable = atacableHashMap.get(new Posicion(unAtacable.getPosicion().getX(), i).toString());
         }
+
+		//Ataco adelante segun mi perspectiva
+		 i = unAtacable.getPosicion().getY() - 1;
+		 atacable = atacableHashMap.get(new Posicion(unAtacable.getPosicion().getX(), i).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i--;
+			atacable = atacableHashMap.get(new Posicion(unAtacable.getPosicion().getX(), i).toString());
+		}
+
+		//Ataco derecha segun mi perspectiva
+		i = unAtacable.getPosicion().getX() - 1;
+		atacable = atacableHashMap.get(new Posicion(i, unAtacable.getPosicion().getY()).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i--;
+			atacable = atacableHashMap.get(new Posicion(i, unAtacable.getPosicion().getY()).toString());
+		}
+
+		//Ataco izquierda segun mi perspectiva
+		i = unAtacable.getPosicion().getX() + 1;
+		atacable = atacableHashMap.get(new Posicion(i, unAtacable.getPosicion().getY()).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i++;
+			atacable = atacableHashMap.get(new Posicion(i, unAtacable.getPosicion().getY()).toString());
+		}
+
+		//Ataco adelante a derecha segun mi perspectiva
+		i = unAtacable.getPosicion().getX() - 1;
+		int j = unAtacable.getPosicion().getY() - 1;
+		atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i--;
+			j--;
+			atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		}
+
+		//Ataco atras a izquierda segun mi perspectiva
+		i = unAtacable.getPosicion().getX() + 1;
+		j = unAtacable.getPosicion().getY() + 1;
+		atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i++;
+			j++;
+			atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		}
+
+		//Ataco adelante a izquierda segun mi perspectiva
+		i = unAtacable.getPosicion().getX() + 1;
+		j = unAtacable.getPosicion().getY() - 1;
+		atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i++;
+			j--;
+			atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		}
+
+		//Ataco atras a derecha segun mi perspectiva
+		i = unAtacable.getPosicion().getX() - 1;
+		j = unAtacable.getPosicion().getY() + 1;
+		atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		while (atacable != null){
+			ataque.atacar(posicion.getX(),posicion.getY(), atacable);
+			i--;
+			j++;
+			atacable = atacableHashMap.get(new Posicion(i, j).toString());
+		}
 	}
 }
