02f5aac769d4427fa0c609eb03e60bdda0f4971f
Elvis <eclaros@fi.uba.ar>
2019-11-28 15:36:57 -0300

    inplementacion y test de tablero.atacanteAtacarAtacable

--- src/main/java/fiuba/algo3/algoChess/modelo/entidades/interfaces/Atacador.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/entidades/interfaces/Atacador.java
@@ -1,7 +1,7 @@
 package fiuba.algo3.algoChess.modelo.entidades.interfaces;
 
 import fiuba.algo3.algoChess.modelo.jugador.Jugador;
-
+import fiuba.algo3.algoChess.modelo.tablero.Posicion;
 
 
 import java.util.ArrayList;
@@ -9,4 +9,5 @@ import java.util.ArrayList;
 
 public interface Atacador {
     public void atacarAtacable(Jugador atacante, Atacable unAtacable, ArrayList<Atacable> atacables);
+    public Posicion getPosicion();
 }
