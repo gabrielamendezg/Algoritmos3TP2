8eaea4a828544450b175d0ecaa95937d00e66bbf
Gabriela Méndez <gabrielamendezg@outlook.com>
2019-12-02 01:23:25 -0300

    Imagenes de Entidades, Menu inicial y Reglas de juego.

---
+++ src/main/java/fiuba/algo3/algoChess/vista/Boton.java
@@ -0,0 +1,23 @@
+package fiuba.algo3.algoChess.vista;
+
+import javafx.scene.control.Button;
+
+public class Boton extends Button {
+	public Boton(String texto, String colorTexto, String color1, String color2) {
+		super();
+		this.setText(texto);
+		this.aplicarEstilo(colorTexto,color1,color2);
+	}
+	
+	public void aplicarEstilo(String colorTexto, String color1, String color2) {
+		this.setStyle("-fx-padding: 8 15 15 15;"+
+	    "-fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;"+
+	    "-fx-background-radius: 8;"+
+	    "-fx-background-color:"+ color1+", "+color2+"; "+
+	    "-fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );"+
+	    "-fx-text-fill: white;"+
+	    "-fx-font-weight: bold;"+
+	    "-fx-font-size: 1.1em;");
+	}
+	//#a34313, #DB5220
+}
--- src/main/java/fiuba/algo3/algoChess/vista/ImagenTablero.java
+++ src/main/java/fiuba/algo3/algoChess/vista/ImagenTablero.java
@@ -1,5 +1,8 @@
 package fiuba.algo3.algoChess.vista;
 
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class ImagenTablero extends GridPane {
@@ -12,20 +15,46 @@ public class ImagenTablero extends GridPane {
         super();
 
         // initialize 20x20 array of spaces
-        for (int x = 0; x < celdas[0].length; x++)
+        for (int x = 0; x < 10; x++)
         {
-            for (int y = 0; y < celdas[1].length; y++)
+            for (int y = 0; y < 20; y++)
             {
-                ImagenCelda celda = new ImagenCelda(x,y);
-                celda.setStyle("-fx-background-color: #90c0c0; -fx-border-color: #d8f0d8;"
-                		+ " -fx-border-width: 1px");
-                //celda.setStyle("");
-                celda.setPrefHeight(30);
-                celda.setPrefWidth(30);
-                celdas[x][y] = celda;
+                celdas[x][y] = inicializarEstiloCeldaAzul(x,y);
                 this.add(celdas[x][y],x,y);
             }
         }
+        
+        for (int x = 10; x < 20; x++)
+        {
+            for (int y = 0; y < 20; y++)
+            {
+                celdas[x][y] = inicializarEstiloCeldaRojo(x,y);
+                this.add(celdas[x][y],x,y);
+            }
+        }
+        
    }
+	
+	private ImagenCelda inicializarEstiloCeldaAzul(int x, int y) {
+		ImagenCelda celda = new ImagenCelda(x,y);
+		celda.setStyle("-fx-background-color: #8cb5bc; -fx-border-color: #484860;"
+        		+ " -fx-border-width: 1px");
+		celda.setPadding(Insets.EMPTY);
+        //celda.setGraphic(new ImageView(new Image(getClass().getResource("imagenes/SoldadoAzul.png").toExternalForm(),30,30,true,true)));
+        celda.setPrefHeight(35);
+        celda.setPrefWidth(35);
+		return celda;
+	}
+	
+	private ImagenCelda inicializarEstiloCeldaRojo(int x, int y) {
+		ImagenCelda celda = new ImagenCelda(x,y);
+		celda.setStyle("-fx-background-color: #da6166; -fx-border-color: #484860;"
+        		+ " -fx-border-width: 1px");
+		celda.setPadding(Insets.EMPTY);
+        celda.setGraphic(new ImageView(new Image(getClass().getResource("imagenes/CatapultaRojo.png").toExternalForm(),30,30,true,true)));
+        celda.setPrefHeight(35);
+        celda.setPrefWidth(35);
+		return celda;
+	}
+	
 }
-
