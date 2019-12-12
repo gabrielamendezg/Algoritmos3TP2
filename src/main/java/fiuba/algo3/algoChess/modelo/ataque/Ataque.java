27b406e4fb0c604d88f95b9744e995a301d7e91d
Elvis <eclaros@fi.uba.ar>
2019-12-12 09:30:47 -0300

    esta mal la verificacion de distancias

--- src/main/java/fiuba/algo3/algoChess/modelo/ataque/Ataque.java
+++ src/main/java/fiuba/algo3/algoChess/modelo/ataque/Ataque.java
@@ -11,19 +11,19 @@ public abstract class Ataque {
 	 protected int ataque;
 	 
 	 public boolean estaEnRango( int posicionXUnidadAtacante, int posicionYUnidadAtacante,  Atacable unidadAtacada) {
-		 
-		 boolean estaEnRango = false;
+
 		  int distanciaX = Math.abs((unidadAtacada.getPosicion().getX()) - posicionXUnidadAtacante);
 		 int distanciaY = Math.abs((unidadAtacada.getPosicion().getY()) - posicionYUnidadAtacante);
-		 
-		 if ( ( ( (rangoInicial <= distanciaX) && (distanciaX <= rangoFinal) ) || distanciaX == 0)
-				 ||
-			  ( ( (rangoInicial <= distanciaY) && (distanciaY <= rangoFinal)) || distanciaY == 0) ){
-			 
-			 estaEnRango = true;
+
+		 if(distanciaY == 0) {
+			 if ((rangoInicial <= distanciaX) && (distanciaX <= rangoFinal)) return true;
+		 }else if (distanciaX == 0){
+		 	if ((rangoInicial <= distanciaY) && (distanciaY <= rangoFinal)) return true;
+		 }else if (((rangoInicial <= distanciaX) && (distanciaX <= rangoFinal) ) && ((rangoInicial <= distanciaY) && (distanciaY <= rangoFinal))){
+			return true;
 		 }
 
-		 return estaEnRango;
+		 return false;
 	 }
 	 
 	 public void atacar( int posicionXUnidadAtacante, int posicionYUnidadAtacante,  Atacable unidadAtacada) {
